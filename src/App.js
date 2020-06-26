c72931b247b13faa8fc91e51e9c05174512fb432
Baijing Sun <49355516+sunbaijing@users.noreply.github.com>
2020-06-26 00:01:38 -0500

    first commit for block and like page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navigation from './component/Navigation.js';
-import LikeList from './component/LikeList.js';
-import MovieList from './component/MovieList.js';
-import BlockList from './component/BlockList.js'
-import Home from './component/Home.js'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./component/Navigation.js";
+import LikeList from "./component/LikeList.js";
+import MovieList from "./component/MovieList.js";
+import BlockList from "./component/BlockList.js";
+import Home from "./component/Home.js";
 
 function App() {
-
   const [items, setItems] = useState([]);
   const [pagination, setPagination] = useState({
     page: 1,
@@ -17,56 +17,68 @@ function App() {
 
   const [likeList, setLikeList] = useState([]);
   const [blockList, setBlockList] = useState([]);
-
+  const [isLoaded, setLoaded] = useState(false);
   useEffect(() => {
     async function fetchItems() {
       const data = await fetch(
-        `https://api.themoviedb.org/3/movie/top_rated?api_key=767a17491866d99d6e9e4da2bd8f8507&language=en-US&page=${pagination.page}`
+        `https://api.themoviedb.org/3/movie/top_rated?api_key=767a17491866d99d6e9e4da2bd8f8507&language=en-US&page=${
+          pagination.page
+        }`
       );
-  
+
       const items = await data.json();
       setItems([...items.results]);
     }
     fetchItems();
-  }, [pagination.page]); 
+    setLoaded(true);
+  }, [pagination.page]);
 
   const handlePrev = () => {
     setPagination({
       page: pagination.page - 1,
       prev: pagination.page - 1 !== 1 ? false : true
-    })
-  }
+    });
+    setLoaded(false);
+  };
 
   const handleNext = () => {
     setPagination({
       page: pagination.page + 1,
       next: pagination.page + 1 !== 373 ? false : true
-    })
-  }
+    });
+    setLoaded(false);
+  };
 
   const addLike = item => {
     if (likeList.find(movie => movie.id === item.id) === undefined) {
-      setLikeList([...likeList, item])
+      setLikeList([...likeList, item]);
     }
-  }
+  };
 
   const addBlock = item => {
     if (blockList.find(movie => movie.id === item.id) === undefined) {
-      setBlockList([...blockList, item])
+      setBlockList([...blockList, item]);
     }
-  }
+  };
 
-  let blockId = new Set(blockList.map(item => { return item.id }))
-  let filterItems = items.filter(item => !blockId.has(item.id))
+  let blockId = new Set(
+    blockList.map(item => {
+      return item.id;
+    })
+  );
+  let filterItems = items.filter(item => !blockId.has(item.id));
 
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Navigation likeList={likeList} blockList={blockList} />
         <Switch>
-          <Route exact path='/'><Home /></Route>
-          <Route exact path='/movie'>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/movie">
             <MovieList
+              isLoaded={isLoaded}
               pagination={pagination}
               likeList={likeList}
               blockList={blockList}
@@ -77,8 +89,20 @@ function App() {
               filterItems={filterItems}
             />
           </Route>
-          <Route path='/like'><LikeList likeList={likeList} /></Route>
-          <Route path='/block'><BlockList blockList={blockList} /></Route>
+          <Route path="/like">
+            <LikeList
+              likeList={likeList}
+              setLikeList={setLikeList}
+              addBlock={addBlock}
+            />
+          </Route>
+          <Route path="/block">
+            <BlockList
+              blockList={blockList}
+              setBlockList={setBlockList}
+              addLike={addLike}
+            />
+          </Route>
         </Switch>
       </div>
     </Router>
@@ -86,5 +110,3 @@ function App() {
 }
 
 export default App;
-
-
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,13 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 const Home = () => {
-    return (
-        <div>
-            <h1>Home Page:(slides show)</h1>
-
-        </div>
-
-    )
-}
+  return (
+    <div>
+      <h1>Home Page:(slides show)</h1>
+    </div>
+  );
+};
 
 export default Home;
