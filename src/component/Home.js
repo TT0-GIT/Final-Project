177a8d34de93825c768afb2b8b699aefeb230682
TT0-GIT <65796055+TT0-GIT@users.noreply.github.com>
2020-07-01 12:17:20 -0700

    homepage-responsive-fix (#18)
    Co-authored-by: Xiaoting Lin <xiaotinglin@Xiaotings-MacBook-Pro.local>

--- src/component/Home.js
+++ src/component/Home.js
@@ -16,27 +16,28 @@ const Home = props => {
       <div className="home-logo">
         <img className="logo" src={logo} alt="logo" />
       </div>
-      <div className="subject">
-        <h1 className="home-title">Top Rated Movies to Discover</h1>
-        <Link to="/movie">
-          <Button size="lg" variant="success">Explore More...</Button>{' '}
-        </Link>
-      </div>
+      {items.length > 0 &&
+        <div className="subject">
+          <h1 className="home-title">Top Rated Movies to Discover</h1>
+          <Link to="/movie">
+            <Button size="lg" variant="success">Explore More...</Button>{' '}
+          </Link>
+        </div>}
       <Carousel className="slides" activeIndex={index} onSelect={handleSelect}>
         {items.map((it, index) => (
           <Carousel.Item className="slide" key={index}>
-              <img className="home-image poster"
-                src={`https://image.tmdb.org/t/p/w500${it.poster_path}`}
-                alt={it.title}
-              />
-                <img className="home-image backdrop"
-                src={`https://image.tmdb.org/t/p/w500${it.backdrop_path}`}
-                alt={it.title}
-              />
+            <img className="home-image poster"
+              src={`https://image.tmdb.org/t/p/w500${it.poster_path}`}
+              alt={it.title}
+            />
+            <img className="home-image backdrop"
+              src={`https://image.tmdb.org/t/p/w500${it.backdrop_path}`}
+              alt={it.title}
+            />
           </Carousel.Item>
         ))}
       </Carousel>
-        <h3 className="home-subtitle">Explore more to like, block movies and check out movie details.</h3>
+      {items.length > 0 && <h3 className="home-subtitle">Explore more to like, block movies and browse movie details.</h3>}
     </div>
   );
 };
